192383d8a4321c2096119bb9d8ba3e0ea7a60580
高見良/yakimannzyuu <a18dc115@dhw.ac.jp>
2018-08-22 18:58:35 +0900

    素材追加、マップ修正

---
+++ WorkSpace/TAKAMI/test01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test01" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="exptest01.png" trans="00ff00" width="300" height="300"/>
+</tileset>
