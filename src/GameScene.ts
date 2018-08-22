6e6c393ea31e98556f7d713031d17563fb4918eb
kishito <ckishi22@gmail.com>
2018-08-22 18:17:27 +0900

    コミット漏れていたので追加

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -1,28 +1,57 @@
 import TimesteppedScene from "./base/TimesteppedScene";
 
+type Vector2 = { x: number, y: number };
+//ベクトルを足す
+function addVectors(a: Vector2, b: Vector2): Vector2 {
+	return { x: a.x + b.x, y: a.y + b.y };
+}
+
+//ベクトルをかける
+function multVector(a: Vector2, s: number): Vector2 {
+	return { x: a.x * s, y: a.y * s };
+}
 export default class GameScene extends TimesteppedScene {
 	private player: Phaser.Sprite;
+	private map: Phaser.Tilemap;
+	private layer: Phaser.TilemapLayer;
 	private cursors: Phaser.CursorKeys;
 
 	private moveSpeed:number;
 	private shotSpeed:number;
+	private scrollSpeed:number;
+
+	private playerPosition:Vector2;
 
 	init(){
 		this.moveSpeed=20;
 		this.shotSpeed=3;
+		this.scrollSpeed=3;
 
 		this.cursors= this.game.input.keyboard.createCursorKeys();
+
+		this.playerPosition ={x:this.game.width / 2 ,y: this.game.height / 2+30};
+
 	}
 
 	preload() {
 		this.game.load.image('player', 'assets/player.png');
 
+		this.game.load.tilemap('map', 'assets/json/super_mario.json',null, Phaser.Tilemap.TILED_JSON); // タイルマップのjsonファイル
+  		this.game.load.image('tiles', 'assets/super_mario.png');  // タイルセット画像ファイル
 	}
 
 	create() {
 		//this.game.stage.backgroundColor = '#787878';
+		
+		this.map = this.game.add.tilemap('map');  // 引数は preload でロードしたマップ名
+		//this.map.addTilesetImage('tileset', 'tiles');  // 第1引数にタイルセット名
 
-		this.player = this.game.add.sprite(this.game.width / 2, this.game.height / 2+30, 'player');
+		this.map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
+		//this.layer = this.map.createLayer('ground');  // タイルマップのレイヤー名
+		this.layer = this.map.createLayer('World1');
+		this.layer.resizeWorld();
+
+		this.player = this.game.add.sprite(this.playerPosition.x , this.playerPosition.y, 'player');
 		this.player.smoothed = false;
 		this.player.anchor.set(0.5, 0.5);
 		this.player.scale.set(2, 2);
@@ -32,20 +61,26 @@ export default class GameScene extends TimesteppedScene {
 	}
 
 	fixedUpdate(dt: number) {
-		//		this.layer.scrollX=this.layer.scrollX+10;
-		//		this.layer.scrollFactorX=10;
-		//		this.layer.angle=10;
+//			this.layer.scrollX=this.layer.scrollX+10;
+	//		this.layer.scrollFactorX=10;
+	//		this.layer.angle=10;
+
+
 
 		//プレイヤーの移動操作
 		this.playerOperation();
+
 		//キャラクター移動範囲制御
 		this.limitedMoveArea();
 
-		//this.layer.cameraOffset.x=this.layer.cameraOffset.x+1;
-		//console.log("layer"+this.layer.x);
+		//キャラ座標とカメラ座標から、キャラの描画先を決める
+		this.player.x = this.playerPosition.x + this.game.camera.x;
+		this.player.y = this.playerPosition.y;
 
-//		this.player.x=this.player.x+1;
-//		console.log("Playerx"+this.player.x);
+		console.log("◆"+this.player.x+"/"+this.playerPosition.x);
+
+//		this.layer.cameraOffset.x=this.layer.cameraOffset.x+1;
+		this.game.camera.x+=this.scrollSpeed;
 
 	}
 
@@ -53,23 +88,23 @@ export default class GameScene extends TimesteppedScene {
 		if (this.cursors.left.isDown)
 		{
 			//console.log("左に移動");//
-			this.player.x = this.player.x-this.moveSpeed;
+			this.playerPosition.x -= this.moveSpeed;
 		}
 		else if (this.cursors.right.isDown)
 		{
 			//console.log("右に移動");//
-			this.player.x = this.player.x+this.moveSpeed;
+			this.playerPosition.x += this.moveSpeed;
 		}
 		
 		if (this.cursors.up.isDown)
 		{
 			//console.log("上に移動");//
-			this.player.y = this.player.y-this.moveSpeed;
+			this.playerPosition.y -= this.moveSpeed;
 		}
 		else if (this.cursors.down.isDown)
 		{
 			//console.log("下に移動");//
-			this.player.y = this.player.y+this.moveSpeed;
+			this.playerPosition.y += this.moveSpeed;
 		}
 		else
 		{
@@ -80,11 +115,23 @@ export default class GameScene extends TimesteppedScene {
 
 	limitedMoveArea() {
 
-		if (this.player.x<0+(this.player.width/2)) this.player.x=0+(this.player.width/2);
-		if (this.player.y<0+(this.player.height/2)) this.player.y=0+(this.player.height/2);
-		if (this.player.x>this.game.width-(this.player.width/2)) this.player.x=this.game.width-(this.player.width/2);
-		if (this.player.y>this.game.height-(this.player.height/2)) this.player.y=this.game.height-(this.player.height/2);
 		
+		if (this.playerPosition.x < 0+(this.player.width/2)){
+			this.playerPosition.x = 0+(this.player.width/2);
+		}
 
+		if (this.playerPosition.y<0+(this.player.height/2)){
+			this.playerPosition.y=0+(this.player.height/2);
+		}
+
+		if (this.playerPosition.x >this.game.width-(this.player.width/2)) {
+			this.playerPosition.x = this.game.width-(this.player.width/2);
+		}
+
+		if (this.playerPosition.y>this.game.height-(this.player.height/2)){
+			this.playerPosition.y=this.game.height-(this.player.height/2);
+		} 
+		
 	}
 }
+
