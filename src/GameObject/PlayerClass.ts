2fcc8d74c8fbebeb4bb6689dde71c7a278686d41
kishito <ckishi22@gmail.com>
2018-08-24 12:28:43 +0900

    長かった式を短く可読性を向上。インデント修正。使っていないelseを削除

--- src/GameObject/PlayerClass.ts
+++ src/GameObject/PlayerClass.ts
@@ -4,102 +4,101 @@ import { Vector2 } from "../base/Mass";
 
 export class PlayerClass {
 
-    public playerSprite:Phaser.Sprite;
+	public playerSprite: Phaser.Sprite;
 	private cursors: Phaser.CursorKeys;
 
-    private game: Game;
-    private mySprite: Phaser.Sprite;
+	private game: Game;
+	private mySprite: Phaser.Sprite;
 
-    private playerPosition:Vector2;
+	private playerPosition: Vector2;
 
-    get width(): number {
-        return this.mySprite.width;
-    }
+	get width(): number {
+		return this.mySprite.width;
+	}
 
-    get height(): number {
-        return this.mySprite.width;
-    }
+	get height(): number {
+		return this.mySprite.width;
+	}
 
-    constructor(game: Game) {
-        this.game = game;
-        this.playerPosition= { x: this.game.width / 2, y: this.game.height / 2 + 30 };
-    }  
+	constructor(game: Game) {
+		this.game = game;
+		this.playerPosition = { x: this.game.width / 2, y: this.game.height / 2 + 30 };
+	}
 
-    createPlayer(){
-        
-        this.mySprite = this.game.add.sprite(this.playerPosition.x , this.playerPosition.y, 'playerSprite');
-        this.mySprite.smoothed = false;
+	createPlayer() {
+
+		this.mySprite = this.game.add.sprite(this.playerPosition.x, this.playerPosition.y, 'playerSprite');
+		this.mySprite.smoothed = false;
 		this.mySprite.anchor.set(0.5, 0.5);
 		this.mySprite.scale.set(2, 2);
 
-        this.cursors= this.game.input.keyboard.createCursorKeys();
+		this.cursors = this.game.input.keyboard.createCursorKeys();
 
-		this.playerPosition ={x:this.game.width / 2 ,y: this.game.height / 2+30};
+		this.playerPosition = { x: this.game.width / 2, y: this.game.height / 2 + 30 };
 
-        //console.log("createPlayer DONE");
-    }
+		//console.log("createPlayer DONE");
+	}
 
-    update(){
-        this.playerOperation();
-        this.limitedPlayerMoveArea();
-        
-        //キャラ座標とカメラ座標からキャラの描画先を決める
-	    this.mySprite.x = this.playerPosition.x + this.game.camera.x;
-        this.mySprite.y = this.playerPosition.y;
-    }
+	update() {
+		this.playerOperation();
+		this.limitedPlayerMoveArea();
 
-    /**
-	 * プレイヤーの移動操作
-	 * キーの入力による座標移動や、攻撃など遊び手がplayerに対して介入する物を書く
-	 */
-	playerOperation(){
-        const moveSpeed=20;
+		//キャラ座標とカメラ座標からキャラの描画先を決める
+		this.mySprite.x = this.playerPosition.x + this.game.camera.x;
+		this.mySprite.y = this.playerPosition.y;
+	}
 
-        if (this.cursors.left.isDown)
-		{
-            //console.log("左に移動");
+	/**
+ * プレイヤーの移動操作
+ * キーの入力による座標移動や、攻撃など遊び手がplayerに対して介入する物を書く
+ */
+	playerOperation() {
+		const moveSpeed = 20;
+
+		if (this.cursors.left.isDown) {
+			//console.log("左に移動");
 			this.playerPosition.x -= moveSpeed;
 		}
-		else if (this.cursors.right.isDown)
-		{
+		else if (this.cursors.right.isDown) {
 			//console.log("右に移動");
 			this.playerPosition.x += moveSpeed;
 		}
-		
-		if (this.cursors.up.isDown)
-		{
+
+		if (this.cursors.up.isDown) {
 			//console.log("上に移動");
 			this.playerPosition.y -= moveSpeed;
 		}
-		else if (this.cursors.down.isDown)
-		{
+		else if (this.cursors.down.isDown) {
 			//console.log("下に移動");
 			this.playerPosition.y += moveSpeed;
-		}
-		else
-		{
-			//入力なし＝待機
 		}
 	}
 
 
-    limitedPlayerMoveArea() {
+	limitedPlayerMoveArea() {
 
-		if (this.playerPosition.x < 0+(this.mySprite.width/2)){
-			this.playerPosition.x = 0+(this.mySprite.width/2);
+		const { playerPosition, game } = this;
+
+		const leftBoundary = this.width / 2;
+		const rightBoundary = game.width - leftBoundary;
+		const topBoundary = this.height / 2;
+		const bottomBoundary = game.height - topBoundary;
+
+		if (this.playerPosition.x < leftBoundary) {
+			this.playerPosition.x = leftBoundary;
 		}
 
-		if (this.playerPosition.y<0+(this.mySprite.height/2)){
-			this.playerPosition.y=0+(this.mySprite.height/2);
+		if (this.playerPosition.y < topBoundary) {
+			this.playerPosition.y = topBoundary;
 		}
 
-		if (this.playerPosition.x >this.game.width-(this.mySprite.width/2)) {
-			this.playerPosition.x = this.game.width-(this.mySprite.width/2);
+		if (this.playerPosition.x > rightBoundary) {
+			this.playerPosition.x = rightBoundary;
 		}
 
-		if (this.playerPosition.y>this.game.height-(this.mySprite.height/2)){
-			this.playerPosition.y=this.game.height-(this.mySprite.height/2);
-		} 		
+		if (this.playerPosition.y > bottomBoundary) {
+			this.playerPosition.y = bottomBoundary;
+		}
 	}
 }
 
--- src/GameObject/TilesetClass.ts
+++ src/GameObject/TilesetClass.ts
@@ -2,24 +2,24 @@ import { Game } from "phaser-ce";
 
 export class TilesetClass {
 
-    private game: Game;
-    
-    private map: Phaser.Tilemap;
+	private game: Game;
+
+	private map: Phaser.Tilemap;
 	private layer: Phaser.TilemapLayer;
 
-    constructor(game: Game) {
-        this.game = game;
-//        this.playerPosition= { x: this.game.width / 2, y: this.game.height / 2 + 30 };
-    }  
+	constructor(game: Game) {
+		this.game = game;
+		//console.log("TilesetClass.constructor DONE");
+	}
 
-    createTileset(){
+	createTileset() {
 		this.map = this.game.add.tilemap('map');
 		this.map.addTilesetImage('test01', 'tiles');
 		this.layer = this.map.createLayer('ground');
 		this.layer.resizeWorld();
-    }
+	}
 
-    update(){
-        
-    }
+	update() {
+
+	}
 }
