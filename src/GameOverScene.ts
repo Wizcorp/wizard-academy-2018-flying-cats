1649a7ef62215e56c84adc07cabd5f8060a71cec
kishito <ckishi22@gmail.com>
2018-08-27 11:28:12 +0900

    ゲームオーバー画面もボタン以外で遷移できるように変更

--- src/GameOverScene.ts
+++ src/GameOverScene.ts
@@ -7,26 +7,25 @@ export default class GameOverScene extends TimesteppedScene {
 	 */
 	preload() {
 		this.game.load.spritesheet('startButton', 'assets/startButton.png', 200, 40);
-		this.game.load.spritesheet('startBg', 'assets/gameOverBackground.png', 768, 432);
+		this.game.load.spritesheet('gameOverBg', 'assets/gameOverBackground.png', 768, 432);
 	}
 
 	/**
 	 * Ran once at initialization.
 	 */
 	create() {
-		/*
-		const title = this.game.add.text(this.game.width / 2, 200, 'Wizard Academy Space Shooter');
-		title.anchor.set(0.5, 0.5);
-		title.align = 'center';
-		title.font = 'Arial';
-		title.fontSize = 45;
-		title.fill = '#ffffff';
-		*/
 
-		const Bg = this.add.image(0,0,"startBg");
+		const Bg = this.add.image(0,0,"gameOverBg");
 
 		const button = this.game.add.button(this.game.width / 2, 300, 'startButton', this.buttonOnClick, this, 2, 1, 0);
 		button.anchor.set(0.5, 0.5);
+
+		const onMouseDown = () => {
+			this.buttonOnClick();
+			this.game.canvas.removeEventListener('mousedown', onMouseDown);
+		};
+
+		this.game.canvas.addEventListener('mousedown', onMouseDown);
 	}
 
 	/**
