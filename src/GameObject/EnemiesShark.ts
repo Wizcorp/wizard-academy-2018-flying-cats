d866f9c6917050a1f2796b380f48ae7ddf0946e2
高見良/yakimannzyuu <a18dc115@dhw.ac.jp>
2018-08-28 18:43:27 +0900

    shark忘れ追加

--- src/GameObject/EnemiesShark.ts
+++ src/GameObject/EnemiesShark.ts
@@ -1,22 +1,25 @@
 import EnemiesBase from "./EnemiesBase";
 import { Game } from "phaser-ce";
 import { PlayerClass } from "./PlayerClass";
+import { EnemiesManager } from "./EnemiesManager";
 
 export class EnemiesShark extends EnemiesBase {
 
+	enemiesManager: EnemiesManager;
 	aTime: number = 0;
 	mTime: number = 60;
-	constructor(game: Game, player: PlayerClass, posX: number, posY: number) {
+	constructor(game: Game, player: PlayerClass, posX: number, posY: number, EM: any) {
 		super(game, player, "enemyC", posX, posY, 50);
+		this.enemiesManager = EM;
 	}
 
 	update() {
 		this.sprite.y += this.fn_MMN(this.player.mySprite.y - this.sprite.y, -2)
 		this.sprite.x += this.fn_MMN(3, 0, this.game.camera.x - this.sprite.x + 700);
-		this.aTime += 1;
+		this.aTime -= 1;
 		if (this.aTime < 0){
 			this.aTime = this.mTime;
-			
+			this.enemiesManager.addEnemy(Math.random()*4,this.sprite.x,this.sprite.y);
 		}
 	}
 
@@ -24,4 +27,8 @@ export class EnemiesShark extends EnemiesBase {
 		x = Math.max(Math.min(x, z), y);
 		return x;
 	}
+
+    delete() {
+		
+    }
 }
