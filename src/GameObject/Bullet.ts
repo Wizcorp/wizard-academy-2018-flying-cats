465fea31d9e6c22075d323bd66c6fc1116a91f33
kishito <ckishi22@gmail.com>
2018-09-06 14:50:46 +0900

    プレイヤーとバレットの移動にDeltaTimeを適用させて、ゲーム進行速度がFPSの影響を受けないようにした（ほんとは敵もやらないと絶対だめ。敵と自分の移動速度差ができてしまう）

--- src/GameObject/Bullet.ts
+++ src/GameObject/Bullet.ts
@@ -8,12 +8,12 @@ export default class Bullet extends Phaser.Sprite {
 		super(game, x, y, imageName);
 		this.anchor.set(0.5, 0.5);
 		this.game.add.existing(this);
-		this.moveSpeed = 10;
+		this.moveSpeed = 650;
 		this.game.physics.arcade.enable(this);
 	}
 
-	fixedUpdate() {
-		this.x += this.moveSpeed;
+	fixedUpdate(dt: number) {
+		this.x += this.moveSpeed * dt;
 	}
 
 	destroy() {
